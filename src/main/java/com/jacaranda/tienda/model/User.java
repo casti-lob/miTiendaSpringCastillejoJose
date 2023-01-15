9a7794acedb802dbeb21f0da129a486352076547
casti-lob <casti_lob@hotmail.com>
2023-01-15 15:21:16 +0100

    intento seguridad

--- src/main/java/com/jacaranda/tienda/model/User.java
+++ src/main/java/com/jacaranda/tienda/model/User.java
@@ -1,8 +1,14 @@
 package com.jacaranda.tienda.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -10,36 +16,48 @@ import jakarta.persistence.Table;
 
 @Entity
 
-public class User {
+public class User implements UserDetails {
 	@Id
 	private String user;
 	private String password;
 	private String name;
 	private String email;
-	private boolean admin;
+	private boolean enabled;
+	private String role;
+	private String verification_code;  
 	
 	//Constructor completo
-	public User(String user, String password, String name, String email, boolean admin) throws UserException {
+	public User(String user, String password, String name, String email, boolean enabled, String role,
+			String verification_code) {
 		super();
-		this.setUser(user);
-		this.setPassword(password);
-		this.setName(name);
-		this.setEmail(email);
-		this.admin = false;
-	}
-	//Constructor sin PK
-	public User(String password, String name, String email, boolean admin) throws UserException {
-		super();
-		this.setPassword(password);
-		this.setName(name);
-		this.setEmail(email);
-		this.admin = false;
+		this.user = user;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.enabled = enabled;
+		this.role = role;
+		this.verification_code = verification_code;
 	}
 	
+	//Constructor sin PK
+	public User(String password, String name, String email, boolean enabled, String role, String verification_code) {
+		super();
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.enabled = enabled;
+		this.role = role;
+		this.verification_code = verification_code;
+	}
+	
+	
+
 	//Constructor vacio
 	public User() {
 		super();
 	}
+	
+
 	public String getUser() {
 		return user;
 	}
@@ -51,9 +69,32 @@ public class User {
 		}
 		
 	}
-	public String getPassword() {
-		return password;
+	
+	public boolean isEnabled() {
+		return enabled;
 	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = "USER";
+	}
+
+	public String getVerification_code() {
+		return verification_code;
+	}
+
+	public void setVerification_code(String verification_code) {
+		this.verification_code = verification_code;
+	}
+
+	
 	public void setPassword(String password) throws UserException {
 		if(password==null||password.isEmpty()) {
 			throw new UserException("El campo password es obligatorio");
@@ -86,10 +127,10 @@ public class User {
 		
 	}
 	public boolean isAdmin() {
-		return admin;
+		return enabled;
 	}
 	public void setAdmin(boolean admin) {
-		this.admin = admin;
+		this.enabled = admin;
 	}
 	
 	@Override
@@ -106,6 +147,41 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(user, other.user);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		authorities.add(new SimpleGrantedAuthority(this.role));
+		return authorities;
+	}
+	
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+	@Override
+	public String getUsername() {
+		
+		return this.user;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return enabled;
 	}
 	
 	
--- src/main/java/com/jacaranda/tienda/service/UserService.java
+++ src/main/java/com/jacaranda/tienda/service/UserService.java
@@ -1,40 +1,114 @@
 package com.jacaranda.tienda.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jacaranda.tienda.model.User;
+import com.jacaranda.tienda.model.UserException;
 import com.jacaranda.tienda.repository.UserRepository;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import net.bytebuddy.utility.RandomString;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository repository;
-	
+	@Autowired
+	private JavaMailSender mailSender;
+
 	public User getUser(String user) {
 		return repository.findById(user).orElse(null);
 	}
-	
-	public List<User> getUsers(){
+
+	public List<User> getUsers() {
 		return repository.findAll();
 	}
-	
+
 	public User addUser(User u) {
 		return repository.save(u);
 	}
-	
+
 	public void deleteUser(User u) {
 		repository.delete(u);
 	}
-	
+
 	public User updateUser(User u) {
-		if(getUser(u.getUser())!=null) {
+		if (getUser(u.getUser()) != null) {
 			return repository.save(u);
-		}else {
+		} else {
 			return null;
 		}
+	}
+
+	public boolean checkExist(User s) {
+		boolean resultado = false;
+		// Comprueba que no existe el nombre del usuario
+		User checkUser = repository.findById(s.getUser()).orElse(null);
+		// Comprueba que no existe el email en la base de datos
+		List<User> checkEmail = repository.findByEmail(s.getEmail());
+		if (checkUser == null && checkEmail.size() == 0) {
+			resultado = true;
+		}
+		return resultado;
+	}
+
+	public void add(User s, String siteURL) throws UnsupportedEncodingException, MessagingException {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(s.getPassword());
+		try {
+			s.setPassword(encodedPassword);
+		} catch (UserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// Genera una cadena aleatoria que guarderemos en el código
+		// de verificación y que le enviaremos al usuario para saber
+		// que es él.
+		String randomCode = RandomString.make(64);
+		s.setVerification_code(randomCode);
+		;
+		s.setEnabled(false);
+		s.setRole("USER");
+		repository.save(s);
+		sendVerificationEmail(s, siteURL);
+	}
+
+	private void sendVerificationEmail(User user, String siteURL)
+			throws MessagingException, UnsupportedEncodingException {
+		String toAddress = user.getEmail();
+		String fromAddress = "romandos962m@gmail.com";
+		String senderName = "Tu Fishing de Confianza";
+		String subject = "Please verify your registration";
+		String content = "Dear [[user]],<br>" + "Please click the link below to verify your registration:<br>"
+				+ "<h3><a href=\"[[URL]]\"target=\"_self\">VERIFY</a></h3>" + "Thank you,<br>" + "Your company name.";
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		helper.setFrom(fromAddress, senderName);
+		helper.setTo(toAddress);
+		helper.setSubject(subject);
+		content = content.replace("[[user]]", user.getUser());
+		String verifyURL = siteURL + "/verify?code=" + user.getVerification_code();
+		content = content.replace("[[URL]]", verifyURL);
+		helper.setText(content, true);
+		mailSender.send(message);
+	}
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findById(username).orElse(null);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return user;
 	}
 }
